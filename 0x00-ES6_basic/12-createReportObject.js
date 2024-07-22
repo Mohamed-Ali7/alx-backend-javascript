0f18b4319827d7b0f0309f125ff53bf71ea9bfc2
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-07-23 02:37:06 +0300

    Adding 12-createReportObject.js and 12-main.js files

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(departments) {
+      return Object.keys(departments).length;
+    },
+  };
+}
