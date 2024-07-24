68dc84789a6b2bb7c16185939356dd9a73df5be3
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-07-25 02:57:58 +0300

    Adding 3-all.js and 3-main.js files

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((resolved) => console.log(`${resolved[0].body} \
+${resolved[1].firstName} \
+${resolved[1].lastName}`));
+}
