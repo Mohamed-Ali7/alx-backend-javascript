009dba35b68eb3576167df20dcd6e517273022d4
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-07-25 03:47:02 +0300

    Updating 6-final-user.js file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+    .then((results) => results.map((r) => ({
+      status: r.status,
+      value: r.status === 'fulfilled' ? r.value : r.reason,
     })));
 }
