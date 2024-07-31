076d00a33f0fc7808d5f77a1c3939dfea78edc0b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-07-31 04:39:47 +0300

    Updating 8-clean_set.js file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
   const result = [];
+
+  if (!startString) {
+    return '';
+  }
   for (const ele of set) {
     if (ele.startsWith(startString)) {
       result.push(ele.slice(startString.length));
