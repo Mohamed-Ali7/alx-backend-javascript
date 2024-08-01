36ac3442549edd948ba7d3ad4ba218b735b7c495
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-01 05:26:30 +0300

    Adding task_4 and task_5

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,49 +1,15 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+export interface MajorCredits {
+  credits: number & { __brand: 'MajorCredits.credits' };
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+export interface MinorCredits {
+  credits: number & { __brand: 'MinorCredits.credits' };
 }
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-
-interface Student {
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-interface StudentConstructor {
-  (firstName: string, lastName: string): Student;
-}
-
-class StudentClass {
-  firstName: string;
-  lastName: string;
-  
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework(): string {
-    return 'Currently working';
-  }
-  
-  displayName(): string {
-    return this.firstName;
-  }
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
