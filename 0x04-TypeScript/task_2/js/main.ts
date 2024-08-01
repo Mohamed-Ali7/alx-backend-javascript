61c5f45321d8da623b916ee8915c9788e85a2604
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-01 04:44:47 +0300

    Updating task_2/js/main.ts file

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -56,3 +56,15 @@ function executeWork(employee: Director | Teacher): string {
     return employee.workTeacherTasks();
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    return 'Invalid class';
+  }
+}
